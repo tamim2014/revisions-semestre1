c52237c6bd6425013b8f83e79d474ef0a3bc171e
Your Name <you@example.com>
2020-03-19 23:44:42 +0100

    Mise à jour Liens pop du semestre1

--- js/menu907.js
+++ js/menu907.js
@@ -1,6 +1,7 @@
         
     function openNav() {
-        document.getElementById("mySidepanel").style.width = "250px";
+        document.getElementById("mySidepanel").style.width = "16%";
+		document.getElementById("mySidepanel").style.left = "0.8%";
     }
 
     function closeNav() {
