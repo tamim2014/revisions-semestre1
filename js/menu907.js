d7b7281d88c57767a25c670c2ab36eebccf7d66f
Your Name <you@example.com>
2020-03-20 09:16:13 +0100

    GO TO TOP with openNav();

--- js/menu907.js
+++ js/menu907.js
@@ -2,6 +2,13 @@
     function openNav() {
         document.getElementById("mySidepanel").style.width = "16%";
 		document.getElementById("mySidepanel").style.left = "0.8%";
+		//remonter le scroll à l'ouverture du menu
+		/*
+		include(top.js);
+		topFunction();
+		* ça ne marche pas: 
+		* solution: i fo mettre topFunction() directement sur l'evenement(dans la page html)
+		*/
     }
 
     function closeNav() {
