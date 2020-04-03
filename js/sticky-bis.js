533dcec516406016f97a6d3b55e751a7deaac5f8
Your Name <you@example.com>
2020-04-03 13:41:50 +0200

    session2017: mise en page

--- js/sticky-bis.js
+++ js/sticky-bis.js
@@ -4,6 +4,7 @@
 	var menu = document.getElementById("menu");
 	var mybutton = document.getElementById("myBtn"); // GO TO TOP
 	var barre = document.getElementById("sidebarGauche");
+	var barreDroite = document.getElementById("sidebarDroit");// session 2017 seulement
 
 	var positionInitiale = ejectBtn.offsetTop;
 
@@ -12,10 +13,12 @@
 		ejectBtn.classList.add("sticky-ejectBtn");
 		menu.classList.add("sticky-menu");
 		barre.classList.add("stickySidebar");// pour les pages corrigées d'examen
+		barreDroite.classList.add("stickySidebarDroit");// pour la session 2017 seulement
 	  } else {
 		ejectBtn.classList.remove("sticky-ejectBtn");
 		menu.classList.remove("sticky-menu");
         barre.classList.remove("stickySidebar"); // pour les pages corrigées d'examen		
+	    barreDroite.classList.remove("stickySidebarDroit"); // pour la session 2017 seulement
 	  }
 	} 
 	
