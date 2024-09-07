102709c65e2e846807131f74c3c4d9d65155f0e6
unknown <a.andjib@yahoo.fr>
2024-09-07 14:19:27 +0200

    Transformation du menu vertical en panel horizontal

--- js/menu907.js
+++ js/menu907.js
@@ -1,6 +1,7 @@
         
     function openNav() {
-        document.getElementById("mySidepanel").style.width = "17%";
+        //document.getElementById("mySidepanel").style.width = "17%";
+        document.getElementById("mySidepanel").style.width = "100%";
 		document.getElementById("mySidepanel").style.left = "0.5%";
 		document.getElementById("mySidepanel").style.bordeRadius = "0 0 0 4px !important";
     }
